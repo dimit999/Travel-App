9e780b2c4cfea1849be82479f005c17aed593b62
Ilya_Baklanov <60309676+Ilya-Baklanov@users.noreply.github.com>
2021-03-16 01:06:47 +0400

    feat: add search & countries atlas

---
+++ src/assets/atlases/countriesAtlas.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: '1',
+    img: '',
+    name: 'Russia',
+    capital: 'Moscow',
+  },
+  {
+    id: '2',
+    img: '',
+    name: 'France',
+    capital: 'Paris',
+  },
+  {
+    id: '3',
+    img: '',
+    name: 'Japan',
+    capital: 'Tokyo',
+  },
+];
--- src/components/home/Header/Search.jsx
+++ src/components/home/Header/Search.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Button, FormControl, InputGroup } from 'react-bootstrap';
 
 import styles from '@/components/home/style.scss';
 
-const Search = () => {
+const Search = ({onChange, searchValue}) => {
   const clickHandler = () => {};
 
-  return <div className={styles['search']}>Search</div>;
+  return (
+    <div className={styles['search']}>
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Search..."
+          aria-label="Search"
+          aria-describedby="basic-addon2"
+          autoFocus={true}
+          onChange={onChange}
+          value={searchValue}
+        />
+        <InputGroup.Append>
+          <Button variant="outline-secondary">Search</Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </div>
+  );
 };
 
 export default Search;
